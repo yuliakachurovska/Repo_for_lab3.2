bf6635a270718919c1419111f2895555d4c3a5cf
Ispiran Anna <ispirananna@knu.ua>
2024-02-13 13:48:21 +0200

    add function prime

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils import gcd, degree_of_five
+from utils import gcd, degree_of_five,is_prime
 
 print(degree_of_five(125))
 print(gcd(8, 12))
+print(is_prime(3))
--- utils.py
+++ utils.py
@@ -12,3 +12,11 @@ def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
