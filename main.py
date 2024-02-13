103a2829e6c5b8422ea824894de874b68f34a492
Anna Aleksandrova <anyaaleksandrova1000@gmail.com>
2024-02-13 13:53:46 +0200

    Added factorial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils import gcd, degree_of_five,is_prime
+from utils import gcd, degree_of_five, is_prime, factorial
 
 print(degree_of_five(125))
 print(gcd(8, 12))
 print(is_prime(3))
+print(factorial(5))
--- utils.py
+++ utils.py
@@ -20,3 +20,9 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
